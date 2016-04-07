32adffb4863ccbbe85b651860fdbc045d3914ac7
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-04-08 00:31:19 +0200

    Added production mails setttings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,14 +79,4 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-  address: 'smtp.postmarkapp.com',
-  port: '25',
-  domain: 'heroku.com',
-  user_name: ENV['POSTMARK_API_TOKEN'],
-  password: ENV['POSTMARK_API_TOKEN'],
-  authentication: :cram_md5,
-  enable_starttls_auto: true
-  }
 end
