bfe5647ca08ca19ddbda8210342e8525e3f05972
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-04-12 19:28:06 +0200

    Slack integration and changed LinkedIn connect button

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -33,6 +33,7 @@ class CandidaciesController < ApplicationController
         format.json { render json: @candidacy.errors, status: :unprocessable_entity }
       end
     end
+    slack_notif_new_written_candidacy(@candidacy)
   end
 
   def transfer
@@ -112,5 +113,19 @@ class CandidaciesController < ApplicationController
         :phone_number
       ]
     )
+  end
+
+  def slack_notif_new_written_candidacy(candidacy)
+    volunteer = candidacy.volunteer
+    first_name = volunteer.first_name
+    last_name = volunteer.last_name
+    mission = candidacy.mission.title
+    association = candidacy.mission.nonprofit_profile.name
+    payload = {
+      username:         "Nouvelle candidature!",
+      text:             first_name + " " + last_name + " vient de postuler pour la mission: \"" + mission +"\", pour l'association " + association +". TODO: modérer la candidature."
+    }
+    uri = URI.parse("https://hooks.slack.com/services/#{ENV['SLACK_EMAIL_SIGNUP_TOKEN']}")
+    Net::HTTP.post_form(uri, :payload => JSON.generate(payload))
   end
 end
