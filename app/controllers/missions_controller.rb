073e0c7b454e9b9cffc932d132b18a5c8a2ef1c8
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-04-19 23:07:02 +0200

    Motivation mail only if mission still open

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -41,7 +41,7 @@ class MissionsController < ApplicationController
       @candidacy = current_volunteer.candidacies.where(mission_id: @mission.id).first
       if !@candidacy.nil?
         @candidacy.plus_browse_count!
-        if @candidacy.browse_count == 3
+        if ( @candidacy.browse_count == 3 && @mission.status == "10_open" )
           VolunteerMailer.motivate(@candidacy).deliver_later
           SlackMotivationNotifierJob.perform_later(@candidacy)
         end
