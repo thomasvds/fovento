cc88fcc95581e8592700ee500b1879794747d9f4
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2017-07-07 12:22:38 +0200

    Trying to switch to mailjet

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.postmarkapp.com',
+  address: 'in-v3.mailjet.com',
   port: '25',
-  domain: 'heroku.com',
-  user_name: ENV['POSTMARK_API_TOKEN'],
-  password: ENV['POSTMARK_API_TOKEN'],
-  authentication: :cram_md5,
-  enable_starttls_auto: true
+  domain: 'fovento.be',
+  user_name: ENV['MAILJET_USERNAME'],
+  password: ENV['MAILJET_PASSWORD']
+  # authentication: :cram_md5,
+  # enable_starttls_auto: true
 }
